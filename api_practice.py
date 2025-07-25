dfef0d23ac5a5900542cd30f5bd0d9a89571f520
yobo1999 <yobo1999@users.noreply.github.com>
2025-07-25 12:35:50 -0500

    fixed flask duplicate handeling

--- api_practice.py
+++ api_practice.py
@@ -2,7 +2,7 @@ import requests
 import getpass
 
 url = "http://192.168.80.154:5000/devices"
-device = {"hostname": "Router2", "ip": "192.168.1.2", "ntp_configured": True, "os_version": "16.12.4", "type": "router"}
+new_device = {"hostname": "Router2", "ip": "192.168.1.2", "ntp_configured": True, "os_version": "16.12.4", "type": "router"}
 def authenticate():
     username = input('Enter your username: ')
     password = getpass.getpass("Enter your password: ")
@@ -23,22 +23,26 @@ def get_devices():
 def add_device():
     print("Starting POST")
     username, password = authenticate()
-    post_request = requests.post(url, auth=(username, password), timeout=5, json=device)
+    request = requests.post(url, auth=(username, password), timeout=5, json=new_device)
     try:
-        print(post_request.status_code)
-        if  post_request.status_code == 201:     
+        print(request.status_code)
+        if  request.status_code == 201:     
             print(f"\n\n {username} has added device")
-            print("POST RESPONSE", post_request.json())
+            print("POST RESPONSE", request.json())
+        elif request.status_code == 409:
+            print("duplicate devices cannot be added")
+        elif request.status_code == 401:
+            print("authentication failed")
         else:
-            print(f"Post from {username} could not be completed with error code {post_request.status_code}")
+            print(f"Post from {username} could not be completed with error code {request.status_code}")
         
         
-    except Exception as e:
-        print(f"Unknown error", e)
+    except Exception as error:
+        print(f"Unknown error", error)
     
 
 add_device()
-get_devices()
+# get_devices()
 # get_devices()
 # if __name__ == "__main__":
         # main()
