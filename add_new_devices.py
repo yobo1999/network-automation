221e8ded5f3944f48e25dfb80361ebca2814c51a
yobo1999 <jyoung0604@gmail.com>
2025-08-11 18:51:04 -0400

    Merged to main first on accident

---
+++ add_new_devices.py
@@ -0,0 +1,17 @@
+from api_practice import api_request
+import csv
+
+
+def main():
+    with open("network-automation/enterprise_list.csv", "r") as device:
+        csv_reader = csv.DictReader(device)
+        api = api_request()
+        
+        for line in csv_reader:
+            api.add_device(line)
+            
+    api.get_devices()
+    
+    
+if __name__ == "__main__":
+    main()
