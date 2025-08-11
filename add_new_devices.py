287ebd1169d33b8f37fd84edfd524e49d059dae5
yobo1999 <jyoung0604@gmail.com>
2025-08-11 19:12:08 -0400

    updated dev to current version

--- add_new_devices.py
+++ add_new_devices.py
@@ -9,12 +9,10 @@ def main():
         
         for line in csv_reader:
             api.add_device(line)
-<<<<<<< HEAD
-            
+
     api.get_devices()
     
-=======
->>>>>>> main
     
 if __name__ == "__main__":
     main()
+
